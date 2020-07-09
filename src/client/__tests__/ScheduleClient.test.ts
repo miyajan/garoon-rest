a679bc7b2884583168cb125d3b2dd8b24bfb7057
Miyata Jumpei <miyajan777@gmail.com>
2020-07-09 23:30:26 +0900

    feat: schedule.getFacilityGroups (#38)

--- src/client/__tests__/ScheduleClient.test.ts
+++ src/client/__tests__/ScheduleClient.test.ts
@@ -304,4 +304,25 @@ describe("ScheduleClient", () => {
       expect(mockClient.getLogs()[0].params).toEqual(params);
     });
   });
+
+  describe("getFacilityGroups", () => {
+    const params = {
+      limit: 100,
+      offset: 0,
+    };
+    beforeEach(async () => {
+      await scheduleClient.getFacilityGroups(params);
+    });
+    it("should pass the path to the http client", () => {
+      expect(mockClient.getLogs()[0].path).toBe(
+        "/api/v1/schedule/facilityGroups"
+      );
+    });
+    it("should send a get request", () => {
+      expect(mockClient.getLogs()[0].method).toBe("get");
+    });
+    it("should pass params as a param to the http client", () => {
+      expect(mockClient.getLogs()[0].params).toEqual(params);
+    });
+  });
 });
